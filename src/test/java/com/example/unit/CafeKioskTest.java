6831a38778f2dbb84f48dc404751e73b5ba62865
jk <akdirdl@gmail.com>
2024-04-17 23:37:07 +0900

    Test: Seperate test

--- src/test/java/com/example/unit/CafeKioskTest.java
+++ src/test/java/com/example/unit/CafeKioskTest.java
@@ -26,7 +26,26 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
     }
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano(), 2);
 
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+        assertThat(cafeKiosk.getBeverages().get(1).getName()).isEqualTo("아메리카노");
+    }
+    @Test
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
+    }
     @Test
     void remove() {
         CafeKiosk cafeKiosk = new CafeKiosk();
